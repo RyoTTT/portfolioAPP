211ea720f14cf60bccbc1d0e633fb216a477f976
RTTT <rttt98520@gmail.com>
2023-03-21 14:23:35 +0900

    アタマページ完成

--- components/DetailScoreTabs/Atama.tsx
+++ components/DetailScoreTabs/Atama.tsx
@@ -7,15 +7,15 @@ import {
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
+import Image from "next/image";
 
 const Atama = () => {
   return (
     <>
-      <Box>
-        <Heading>アタマ(雀頭)の加符点</Heading>
+      <Box textAlign="center" marginBottom="3%">
+        <Heading color="red">アタマ(雀頭)の加符点</Heading>
         <Text>
           <Highlight
             query={["0", "2"]}
@@ -23,12 +23,12 @@ const Atama = () => {
           >
             どの種類でアタマを作ったかで、加符点が与えられる。基本的には数牌のアタマでは加符点は0点で、役牌のアタマでは2点の加符点となる。
           </Highlight>
-        </Text>
+        </Text>   
       </Box>
-      <Box>
-        <Text>風牌が2点の加符点となる場合</Text>
-        <Text>
-          アタマの風牌が自風または場風と同じ場合のみ、2点の加符点が与えられる。
+      <Box textAlign="center">
+    <Heading>風牌の加符点について</Heading>
+    <Text>
+        風牌が2点の加符点となる場合→アタマの風牌が自風または場風と同じ場合のみ、2点の加符点が与えられる。
         </Text>
         <TableContainer>
           <Table>
@@ -40,34 +40,110 @@ const Atama = () => {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
+              <Tr fontSize="20px">
                 <Td>場風が「東」、自風が「西」</Td>
-                <Td>画像</Td>
+                <Td display="flex">
+                  <Image
+                    src="/tupai/nann.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                  <Image
+                    src="/tupai/nann.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                </Td>
                 <Td>0点</Td>
               </Tr>
-              <Tr>
+              <Tr fontSize="20px">
                 <Td>場風が「東」、自風が「北」</Td>
-                <Td>画像</Td>
-                <Td>0点</Td>
+                <Td display="flex">
+                  <Image
+                    src="/tupai/pee.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                  <Image
+                    src="/tupai/pee.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                </Td>
+                <Td>2点</Td>
               </Tr>
-              <Tr>
+              <Tr fontSize="20px">
                 <Td>場風が「南」、自風が「東」</Td>
-                <Td>画像</Td>
-                <Td>0点</Td>
+                <Td display="flex">
+                  <Image
+                    src="/tupai/nann.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                  <Image
+                    src="/tupai/nann.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                </Td>
+                <Td>2点</Td>
               </Tr>
-              <Tr>
-                <Td>場風が「南」、自風が「南」</Td>
-                <Td>画像</Td>
-                <Td>0点</Td>
+              <Tr fontSize="20px">
+                <Td>場風が「東」、自風が「東」</Td>
+                <Td display="flex">
+                  <Image
+                    src="/tupai/ton.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                  <Image
+                    src="/tupai/ton.gif"
+                    alt="test"
+                    width={40}
+                    height={40}
+                  ></Image>
+                </Td>
+                <Td>2点(ルールによっては4点)</Td>
               </Tr>
             </Tbody>
           </Table>
         </TableContainer>
       </Box>
-      <Box>
+      <Box textAlign="center" margin="3%">
         <Heading>三元牌がアタマの場合</Heading>
-        <text>画像</text>
-        <Text>三元牌はどのような状況でもアタマにした場合2点の加符点が与えられる。</Text>
+        <Box display="flex" justifyContent="center">
+        <Image src="/tupai/chun.gif" alt="test" width={40} height={40}></Image>
+        <Image src="/tupai/haku.gif" alt="test" width={40} height={40}></Image>
+        <Image src="/tupai/jatsu.gif" alt="test" width={40} height={40}></Image>
+        </Box>
+        <Text>
+          三元牌はどのような状況でもアタマにした場合2点の加符点が与えられる。
+        </Text>
+      </Box>
+      <Box textAlign="center" margin="3%">
+        <Heading>数牌がアタマの場合</Heading>
+        <Box display="flex" justifyContent="center" marginBottom="1%">
+        <Image src="/manzu/m2.gif" alt="test" width={40} height={40}></Image>
+        <Image src="/manzu/m2.gif" alt="test" width={40} height={40}></Image>
+        </Box>
+        <Box display="flex" justifyContent="center" marginBottom="1%">
+        <Image src="/pinzu/p1.gif" alt="test" width={40} height={40}></Image>
+        <Image src="/pinzu/p1.gif" alt="test" width={40} height={40}></Image>
+        </Box>
+        <Box display="flex" justifyContent="center" marginBottom="1%">
+        <Image src="/sozu/s8.gif" alt="test" width={40} height={40}></Image>
+        <Image src="/sozu/s8.gif" alt="test" width={40} height={40}></Image>
+        </Box>
+        <Text>
+          数牌はどのような状況でもアタマにした場合加符点はない。
+        </Text>
       </Box>
     </>
   );
